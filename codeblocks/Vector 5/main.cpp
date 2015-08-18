e870bea3e95332e6a41b45bdc8a8fea26a290bb9
Franz-S <f.stuebler@gmail.com>
2015-08-18 12:01:45 +0200

    Benchmark fertig

--- codeblocks/Vector 5/main.cpp
+++ codeblocks/Vector 5/main.cpp
@@ -1,14 +1,30 @@
+#ifndef VIENNACL_WITH_OPENMP
+//#define VIENNACL_WITH_OPENMP
+#endif
+#include "viennacl/vector.hpp"
 #include "viennacl/linalg/inner_prod.hpp"
 #include "viennacl/linalg/lu.hpp"
 #include "viennacl/tools/timer.hpp"
 #include "viennacl/linalg/norm_1.hpp"
 #include "viennacl/linalg/norm_2.hpp"
 #include "viennacl/linalg/norm_inf.hpp"
+#include "viennacl/linalg/maxmin.hpp"
+#include "viennacl/linalg/sum.hpp"
 
 #include <iomanip>
 #include <stdlib.h>
 #include <fstream>
 #define FILENAME "file.txt"
+
+
+int cmp(const void *x, const void *y)//the comparing for the sort func
+{
+    double xx = *(double*)x, yy = *(double*)y;
+    if (xx < yy) return -1;
+    if (xx > yy) return  1;
+    return 0;
+}
+
 template<class T, class F>
 void init_random(viennacl::matrix<T, F> & M)
 {
@@ -29,7 +45,7 @@ void init_random(viennacl::vector<T> & x)
 }
 
 template<class T>
-void bench(size_t BLAS1_N, std::string const & prefix)
+void bench(size_t BLAS_N, std::string const & prefix,int bereich)
 {
     std::fstream output;
 
@@ -37,88 +53,188 @@ void bench(size_t BLAS1_N, std::string const & prefix)
     using viennacl::linalg::norm_1;
     using viennacl::linalg::norm_2;
     using viennacl::linalg::norm_inf;
+    using viennacl::linalg::index_norm_inf;
+    using viennacl::linalg::max;
+    using viennacl::linalg::min;
+    using viennacl::linalg::sum;
+    using viennacl::linalg::plane_rotation;
+    using viennacl::scalar_vector;
+    using viennacl::linalg::element_fabs;
+    using viennacl::linalg::inclusive_scan;
+    using viennacl::linalg::exclusive_scan;
+    using viennacl::scalar_matrix;
+    using viennacl::linalg::matrix_diagonal_assign;
+    using viennacl::linalg::matrix_diag_from_vector;
+    using viennacl::linalg::matrix_diag_to_vector;
+    using viennacl::linalg::prod;
+    using viennacl::linalg::outer_prod;
+    using viennacl::identity_matrix;
+    using viennacl::diag;
+
+#define A_SIZE 40//the number of iterations for the function
+
     viennacl::tools::timer timer ;
-    double time_previous, time_spent;
-    size_t Nruns;
+    double time_spent_a[A_SIZE];
+    double time_spent=0;
     output.open(FILENAME,std::ios::in|std::ios::out|std::ios::ate);
+
 #define BENCHMARK_OP(OPERATION, NAME, PERF, INDEX) \
-  if(BLAS1_N==0)\
+  if(BLAS_N==0)\
   {\
-    output << std::left << std::setw(8) << prefix + NAME <<",";\
+    output << std::left << std::setw(11) << prefix + NAME <<",";\
   }\
   else{\
   OPERATION; \
   viennacl::backend::finish();\
   timer.start(); \
-  Nruns = 0; \
-  time_spent = 0; \
-  for(int i=1;i<50;i++) \
+  for(int i=0;i<A_SIZE;++i) \
   { \
-    time_previous = timer.get(); \
+    time_spent_a[i] = timer.get(); \
     OPERATION; \
     viennacl::backend::finish(); \
-    time_spent += timer.get() - time_previous; \
-    Nruns+=1; \
+    time_spent_a[i] = timer.get()-time_spent_a[i]; \
   } \
-  time_spent/=(double)Nruns; \
-  output << std::left << std::setw(8) << PERF<<" " ; \
-  }\
+  qsort(time_spent_a, sizeof(time_spent_a)/sizeof(time_spent_a[0]), sizeof(time_spent_a[0]), cmp);\
+  time_spent=time_spent_a[(long)A_SIZE/3]; \
+  output << std::left << std::setw(11) << PERF<<" " ; \
+  }
 
-    //BLAS1
+#define MATRIX_OP \
+        BENCHMARK_OP(A=trans(B),                             "trans",       std::setprecision(3) << double(2*(BLAS_N*BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")\
+        BENCHMARK_OP(A=B*b,                                  "A=B*b",       std::setprecision(3) << double((BLAS_N+2*BLAS_N*BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")\
+        BENCHMARK_OP(A=B*b+C*c,                              "A=B*b+C*c",   std::setprecision(3) << double((2*BLAS_N+3*BLAS_N*BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")\
+        BENCHMARK_OP(A+=B*b+C*c,                             "A+=B*b+C*c",  std::setprecision(3) << double((2*BLAS_N+4*BLAS_N*BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")\
+        BENCHMARK_OP(A = scalar_matrix<T>(BLAS_N,BLAS_N,a),  "A[i][i]=a",   std::setprecision(3) << double((BLAS_N*BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")\
+        BENCHMARK_OP(A=identity_matrix<T>(BLAS_N),           "A.diag=1",    std::setprecision(3) << double((BLAS_N*BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")\
+        BENCHMARK_OP(A=diag(x,0),                            "A.diag=x",    std::setprecision(3) << double((BLAS_N+BLAS_N*BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")\
+        BENCHMARK_OP(x=diag(A),                              "x=A.diag",    std::setprecision(3) << double((BLAS_N+BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")\
+        BENCHMARK_OP(A=element_fabs(B),                      "A=fabs(B)",   std::setprecision(3) << double(2*(BLAS_N*BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")\
+        BENCHMARK_OP(y=prod(A,x),                            "y=prod(A.x)", std::setprecision(3) << double((2*BLAS_N+BLAS_N*BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")\
+        BENCHMARK_OP(A+= a * outer_prod(x,y),                "A.rank1",     std::setprecision(3) << double((2*BLAS_N+2*BLAS_N*BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")\
+
+    switch(bereich)
     {
-        viennacl::scalar<T> s(0);
-        T alpha = (T)2.4;
+    case 0://For Testing
+    {
+        viennacl::matrix<T,viennacl::column_major> A(BLAS_N, BLAS_N);
 
-        viennacl::vector<T> x(BLAS1_N);
-        viennacl::vector<T> y(BLAS1_N);
-        viennacl::vector<T> z(BLAS1_N);
+        viennacl::vector<T> x(BLAS_N);
+
+        init_random(A);
+
+        init_random(x);
+
+        BENCHMARK_OP(A=identity_matrix<T>(BLAS_N), "A.diag=1",    std::setprecision(3) << double((BLAS_N*BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(A=diag(x,0),                  "A.diag=x",    std::setprecision(3) << double((BLAS_N+BLAS_N*BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(x=diag(A),                    "x=A.diag",    std::setprecision(3) << double((BLAS_N+BLAS_N)*sizeof(T))/time_spent * 1e-9, "GB/s")
+
+        break;
+    }
+    case 1:
+    {
+        //Vector vector
+        viennacl::scalar<T> s(0);
+        T a = (T)2.4;
+        T b = (T)3.8;
+
+        long j;
+
+        viennacl::vector<T> x(BLAS_N);
+        viennacl::vector<T> y(BLAS_N);
+        viennacl::vector<T> z(BLAS_N);
+
+        //viennacl::vector_range<viennacl::vector<T> > x_small(x, viennacl::range(1, 10)); how to make a ranged vector
 
         init_random(x);
         init_random(y);
         init_random(z);
 
-
-        BENCHMARK_OP(swap(x,y),            "swap", std::setprecision(3) << double(4*BLAS1_N*sizeof(T))/time_spent * 1e-9, "GB/s")
-        BENCHMARK_OP(y *= alpha,           "y*=a", std::setprecision(3) << double(2*BLAS1_N*sizeof(T))/time_spent * 1e-9, "GB/s")
-        BENCHMARK_OP(y = x,                "y=x", std::setprecision(3) << double(2*BLAS1_N*sizeof(T))/time_spent * 1e-9, "GB/s")
-        BENCHMARK_OP(y += alpha * x,       "y+=a*x", std::setprecision(3) << double(3*BLAS1_N*sizeof(T))/time_spent * 1e-9, "GB/s")
-        BENCHMARK_OP(y -= alpha * x,       "y-=a*x", std::setprecision(3) << double(3*BLAS1_N*sizeof(T))/time_spent * 1e-9, "GB/s")
-        BENCHMARK_OP(s=inner_prod(x,y),    "dot", std::setprecision(3) << double(2*BLAS1_N*sizeof(T))/time_spent * 1e-9, "GB/s")
-        BENCHMARK_OP(alpha = norm_1(x),    "L1", std::setprecision(3) << double(1*BLAS1_N*sizeof(T))/time_spent * 1e-9, "GB/s")
-        BENCHMARK_OP(alpha = norm_2(x),    "L2", std::setprecision(3) << double(1*BLAS1_N*sizeof(T))/time_spent * 1e-9, "GB/s")
-        BENCHMARK_OP(alpha = norm_inf(x),  "Linf", std::setprecision(3) << double(1*BLAS1_N*sizeof(T))/time_spent * 1e-9, "GB/s")
-
-        output.close();
+        BENCHMARK_OP(y = a*x,                        "y=a*x",   std::setprecision(3) << double(2*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(y = x,                          "y=x",     std::setprecision(3) << double(2*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(y = a * x+ b * z,               "y=ax+bz", std::setprecision(3) << double(3*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(y += a * x+ b * z,              "y+=ax+bz",std::setprecision(3) << double(4*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(y = scalar_vector<T>(BLAS_N,a), "y[i]=a",  std::setprecision(3) << double(1*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(swap(x,y),                      "swap",    std::setprecision(3) << double(4*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(y=element_fabs(x),              "fabs(x)", std::setprecision(3) << double(2*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(s=inner_prod(x,y),              "dot",     std::setprecision(3) << double(2*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(a = norm_1(x),                  "L1",      std::setprecision(3) << double(1*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(a = norm_2(x),                  "L2",      std::setprecision(3) << double(1*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(a = norm_inf(x),                "Linf",    std::setprecision(3) << double(1*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(j=index_norm_inf(x),            "index_L", std::setprecision(3) << double(1*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(a=max(x),                       "max",     std::setprecision(3) << double(1*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(a=min(x),                       "min",     std::setprecision(3) << double(1*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(a=sum(x),                       "sum",     std::setprecision(3) << double(1*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(plane_rotation(x, y, a, b),     "pla_rot", std::setprecision(3) << double(4*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(inclusive_scan(x,y),            "in_scan", std::setprecision(3) << double(2*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        BENCHMARK_OP(exclusive_scan(x,y),            "ex_scan", std::setprecision(3) << double(2*BLAS_N*sizeof(T))/time_spent * 1e-9, "GB/s")
+        j++;
+        break;
     }
+    case 2://Matrix vector column mayor
+    {
+
+        viennacl::matrix<T,viennacl::column_major> A(BLAS_N, BLAS_N);
+        viennacl::matrix<T,viennacl::column_major> B(BLAS_N, BLAS_N);
+        viennacl::matrix<T,viennacl::column_major> C(BLAS_N, BLAS_N);
+        viennacl::vector<T> x(BLAS_N);
+        viennacl::vector<T> y(BLAS_N);
+        T a = (T)2.4;
+        T b = (T)3.8;
+        T c = (T)3.8;
+
+        init_random(A);
+        init_random(B);
+        init_random(C);
+        init_random(x);
+        init_random(y);
+        MATRIX_OP
+        break;
+    }
+    case 3://Matric vector with row mayor
+    {
+
+        viennacl::matrix<T,viennacl::row_major> A(BLAS_N, BLAS_N);
+        viennacl::matrix<T,viennacl::row_major> B(BLAS_N, BLAS_N);
+        viennacl::matrix<T,viennacl::row_major> C(BLAS_N, BLAS_N);
+        viennacl::vector<T> x(BLAS_N);
+        viennacl::vector<T> y(BLAS_N);
+
+        T a = (T)2.4;
+        T b = (T)3.8;
+        T c = (T)3.8;
+
+        init_random(A);
+        init_random(B);
+        init_random(C);
+        init_random(x);
+        init_random(y);
+        MATRIX_OP
+        break;
+    }
+    }
+    output.close();
+
 }
 int main(int argc, char *argv[])
 {
-    //fprintf(stdout,"Benchmark : BLAS1_von Franz\n");
+    //argv[1]-->vector or matrix size
+    //argv[2]-->if the operations are(0:testing,1:Vector,2:Matrix coloum mayor,3:Matrix row mayor)
     std::fstream output;
-    std::size_t BLAS1_N = atoi(argv[1]);
-    if(BLAS1_N==0)
+    std::size_t BLAS_N = atoi(argv[1]);
+    if(BLAS_N==0)
     {
         output.open(FILENAME,std::ios::out);
-        output << std::left << std::setw(8) << "N" <<",";\
+        output << std::left << std::setw(11) << "N" <<",";
         output.close();
     }
 
     output.open(FILENAME,std::ios::in|std::ios::out|std::ios::ate);
 
-    if(BLAS1_N!=0) output<< std::setprecision(3) << std::left << std::setw(8) << double(BLAS1_N) <<" ";
+    if(BLAS_N!=0) output<< std::setprecision(3) << std::left << std::setw(11) << double(BLAS_N) <<" ";
     output.close();
-    bench<float>(BLAS1_N, "s.");
-    bench<double>(BLAS1_N, "d.");
+    bench<float>(BLAS_N, "s.",atoi(argv[2]));
+    bench<double>(BLAS_N, "d.",atoi(argv[2]));
     output.open(FILENAME,std::ios::in|std::ios::out|std::ios::ate);
     output << std::endl;
     output.close();
-
 }
-
-
-
-
-
-
-
-
--- workspace/Blas1/src/Blas1_2.py
+++ workspace/Blas1/src/Blas1_2.py
@@ -7,24 +7,50 @@ import subprocess, time, Gnuplot
 import os
 import shutil
 
-print "Benchmarktest von Franz"
 
-directory="/home/franz/Franz/Benchmark/temp/"+"Bench"+time.strftime("%d.%m.%Y__%H_%M_%S")+"/" # current date and time for the directory
+
+op_choice=3#0:testing,1:Vector,2:Matrix coloum mayor,3:Matrix row mayor 
+
+if op_choice==0 :
+    min_loop=10
+    max_loop=5e3
+    foldername="testing"
+elif op_choice==1 :
+    min_loop=100
+    max_loop=1e7
+    foldername="Vector"
+elif op_choice==2 :
+    min_loop=10
+    max_loop=5e3
+    foldername="Matrix coloum mayor"
+elif op_choice==3 :
+    min_loop=10
+    max_loop=5e3
+    foldername="Matrix row mayor"
+    
+directory="/home/franz/Franz/Benchmark/temp/"+foldername+time.strftime("%d.%m.%Y__%H_%M_%S")+"/" # current date and time for the directory
 if not os.path.exists(directory): #if the directory does not exist, it will be created
     os.makedirs(directory)
 filename=directory+("file.txt") #path for datafile
 scriptname=directory+("script.gp")#path for script
+readmename=directory+("readme.txt")#path for readme
 c_file="/home/franz/Franz/codeblocks/Vector 5/bin/Release/Vector"#path for the c++ file
 
+readme=open(readmename,"w")
+readme.write("""OHNE VIENNACL_WITH_OPENMP""")
+readme.close()
+    
+print "Benchmarktest von Franz"
 
-subprocess.call([c_file, str(0)])#c-file aufrufen, es wird im python verzeichnis die file.txt erstellt
+text=subprocess.check_output([c_file, str(0),str(op_choice)])#c-file aufrufen, es wird im python verzeichnis die file.txt erstellt
+print text
 
-
-i=100 #start size for the vector
-while i<1e6: #the loop
-    subprocess.call([c_file, str(i)])
-    i=long(i*2)
+i=min_loop #start size for the vector
+while i<max_loop: #the loop
+    text=subprocess.check_output([c_file, str(i),str(op_choice)])
+    print text
     print str(i)+". size, finished"
+    i=long(i*1.3)
     
 shutil.move(os.path.abspath(".")+"/file.txt",filename)#verschieben des file.txt in den richtigen ordner
 
@@ -34,17 +60,20 @@ axis_titel.pop(0)#erstes element loeschen weil "N"
 
 script=open(scriptname,"w") #the script for gnuplot will be created
 script.write("""set xlabel"N"
-set ylabel "GFlops/sec"
+set ylabel "GB/sec"
 set logscale\n""")
 #set the labeling and the graph to logarithmical 
 i=0
-while i<len(axis_titel): #creat for each data set a independent plot and a eps-file
-    script.write("set terminal x11 "+str(i)+"\n")
-    script.write("plot \""+filename+"\" using 1:"+ str(i+2)+" title '"+axis_titel[i]+"' with linespoints\n")
-    script.write("set terminal postscript eps 25 color solid linewidth 3 enhanced\n")
-    script.write("set output \""+directory+axis_titel[i]+".eps\"\n")
-    script.write("plot \""+filename+"\" using 1:"+ str(i+2)+" title '"+axis_titel[i]+"' with linespoints\n")
-    i=i+1
+if True:
+    while i<len(axis_titel): #creat for each data set a independent plot and a eps-file
+        script.write("set terminal x11 "+str(i)+"\n")
+        script.write("plot \""+filename+"\" using 1:"+ str(i+2)+" title '"+axis_titel[i]+"' with linespoints\n")
+        script.write("set terminal postscript eps 25 color solid linewidth 3 enhanced\n")
+        script.write("set output \""+directory+axis_titel[i]+".eps\"\n")
+        script.write("plot \""+filename+"\" using 1:"+ str(i+2)+" title '"+axis_titel[i]+"' with linespoints\n")
+        i=i+1
+else:
+    script.write("plot \"/home/franz/Franz/Benchmark/file.txt\" u 1:3 title 'd.y=x_old' w lp, \""+filename+"\" u 1:3 title 'd.y=x_new' w lp\n")
 
 script.write("pause -1 \"Hit any key to continue\"\n")
 script.close()
@@ -53,4 +82,3 @@ g = Gnuplot.Gnuplot(debug=0) #start gnuplot with the script
 g.load(scriptname)
 raw_input('Please press return to continue...\n')
 
-axis_titel.close()
