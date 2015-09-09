4e3c1c50330718e35c9cd796853c543918049e52
Franz-S <f.stuebler@gmail.com>
2015-09-09 10:39:47 +0200

    update test.py damit ich auch andere sachen testen kann

--- workspace/Blas1/src/test.py
+++ workspace/Blas1/src/test.py
@@ -6,44 +6,43 @@ Created on Aug 10, 2015
 #ACHTUNG KEINE LEERZEICHEN IM PFAD WEGEN commands
 
 import commands,sys
+print "start"
+
 
 buildfolder="~/viennacl/build/"#path to the build folder for cmake e.g
-
-
+testfile=["vector_float_double-test-cpu","matrix_row_double-test-cpu","matrix_col_double-test-cpu"][2]
 cmake_flag="cmake .. -DENABLE_OPENMP=OFF -DCMAKE_BUILD_TYPE=Release -DENABLE_PEDANTIC_FLAGS=ON -DCMAKE_CXX_FLAGS=-Werror"
-cmd="cd "+buildfolder+";"+cmake_flag+";make vector_float_double-test-cpu -j4"
+cmd="cd "+buildfolder+";"+cmake_flag+";make "+testfile
 text=commands.getstatusoutput(cmd)#cmake set flags and make the file
 print text[1]
 if int(text[0])!=0:#check if the make process has worked
         print text[0]
         print "Error during make :("
         sys.exit()
-cmd="~/viennacl/build/tests/vector_float_double-test-cpu"
-text=commands.getstatusoutput(cmd)#cmake set flags and make the file
+cmd="~/viennacl/build/tests/"+testfile
+text=commands.getstatusoutput(cmd)#start the test
 print text[1]
-if int(text[0])!=0:#check if the make process has worked
+if int(text[0])!=0:#check if the test has worked
         print text[0]
         print "Error during make :("
         sys.exit()
-cmd="~/viennacl/build/tests/vector_float_double-test-cpu"
 
 
 cmake_flag="cmake .. -DENABLE_OPENMP=ON -DCMAKE_BUILD_TYPE=Release -DENABLE_PEDANTIC_FLAGS=ON -DCMAKE_CXX_FLAGS=-Werror"
 
-cmd="cd "+buildfolder+";"+cmake_flag+";make vector_float_double-test-cpu -j4"
+cmd="cd "+buildfolder+";"+cmake_flag+";make "+testfile
 text=commands.getstatusoutput(cmd)#cmake set flags and make the file
 print text[1]
 if int(text[0])!=0:#check if the make process has worked
         print text[0]
         print "Error during make :("
         sys.exit()
-cmd="~/viennacl/build/tests/vector_float_double-test-cpu"
-text=commands.getstatusoutput(cmd)#cmake set flags and make the file
+cmd="~/viennacl/build/tests/"+testfile
+text=commands.getstatusoutput(cmd)#start the test
 print text[1]
-if int(text[0])!=0:#check if the make process has worked
+if int(text[0])!=0:#check if the test has worked
         print text[0]
         print "Error during make :("
         sys.exit()
-cmd="~/viennacl/build/tests/vector_float_double-test-cpu"
 
 print "finished everything"
